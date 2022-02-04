cd53c582d6f594422b9b50d7cb6f5a8e924293be
Dilpreet Singh <dilpreetsingh.ds277@gmail.com>
2022-02-04 14:55:47 -0500

    Six Conversion methods , default and parametrized constructor created

--- Assignment2/Conversion.cs
+++ Assignment2/Conversion.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class Conversion
+    public class Conversion
     {
+        private int tempValue;
+    
+        public Conversion()
+        {
+            this.tempValue = 1;
+        }
+
+        public Conversion(int tempValue)
+        {
+            this.tempValue = tempValue;
+        }
+
+        public double ConvertCelciusToFahrenheit()
+        {
+            return (this.tempValue * 1.8) + 32;
+        }
+        public double ConvertCelciusToKelvin()
+        {
+            return this.tempValue + 273.15;
+        }
+
+        public double ConvertKelvinToFahrenheit()
+
+        {
+            return (this.tempValue * (9 / 5)) - 459.67;
+        }
+        public double ConvertKelvinToCelsius()
+
+        {
+            return this.tempValue - 273.15;
+        }
+        public double ConvertFahrenheitToKelvin()
+
+        {
+            return (this.tempValue - 32) * (5 / 9) + 273.15;
+        }
+        public double ConvertFahrenheitToCelsius()
+
+        {
+            return (this.tempValue - 32) * 0.5556;
+        }
+
+
 
     }
 }
